12d1048796666d6e4678c0e302d0df0eeb7fca29
Anastasia Kosataya <102319144+akosataya@users.noreply.github.com>
2022-10-13 19:36:10 +0400

    prepared everything for the 6th project

---
+++ scripts/validate.js
@@ -0,0 +1,11 @@
+// включение валидации вызовом enableValidation
+// все настройки передаются при вызове
+
+enableValidation({
+    formSelector: '',
+    inputSelector: '',
+    submitButtonSelector: '',
+    inactiveButtonClass: '',
+    inputErrorClass: '',
+    errorClass: ''
+});
