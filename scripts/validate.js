5e2f66e37855eef371494c08dd169fa5793ac051
Anastasia Kosataya <102319144+akosataya@users.noreply.github.com>
2022-10-17 20:29:56 +0400

    added new classes, began to write validation js

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,11 +1,37 @@
 // включение валидации вызовом enableValidation
 // все настройки передаются при вызове
 
-enableValidation({
-    formSelector: '',
-    inputSelector: '',
-    submitButtonSelector: '',
-    inactiveButtonClass: '',
-    inputErrorClass: '',
-    errorClass: ''
-});
+const validationFormList = {
+    formSelector: '.popup__form',
+    inputSelector: '.popup__input',
+    submitButtonSelector: '.popup__save',
+    inactiveButtonClass: 'popup__save_inactive',
+    inputErrorClass: 'popup__input_type-error',
+    errorClass: 'popup__text-error_active'
+};
+
+const enableValidation = (validationFormList) => {
+    const formList = Array.from(document.querySelectorAll(validationFormList.formSelector));
+
+    formList.forEach((form) => {
+        form.addEventListener('submit', function (e) {
+            e.preventDefault();
+        });
+        setEventListeners(form, validationFormList);
+    });
+}
+
+const setEventListeners = (form, validationFormList) => {
+    const inputList = Array.from(form.querySelectorAll(validationFormList.inputSelector));
+    const submitBtn = form.querySelector(validationFormList.submitButtonSelector);
+
+    toggleBtnState(inputList, submitBtn, validationFormList);
+
+    inputList.forEach((inputElement) => {
+        inputElement.addEventListener('input', function() {
+            isValid(form, inputElement, enableValidation);
+            toggleBtnState(inputList, submitBtn, validationFormList);
+        });
+    });
+}
+
