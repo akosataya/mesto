694cedde6797c4e714d1db943d514532531c9fda
Anastasia Kosataya <102319144+akosataya@users.noreply.github.com>
2022-11-17 20:44:51 +0400

    refactor: changed the project structure

---
+++ src/pages/index.js
@@ -0,0 +1,97 @@
+import { initialCards, validationConfig, popupAddForm, formElementEdit, popupList, nameInput, jobInput, profileName, profileAbout, popupEdit, editBtn, photoCardsContainer, photo, caption, popupArea, popupAdd, modalAddBtn, nameAddInput, linkAddInput } from '../utils/constants.js';
+import Card from '../components/Card.js';
+import FormValidator from "../components/FormValidator.js";
+
+
+/** Валидация */
+const editValidator = new FormValidator(validationConfig, formElementEdit);
+editValidator.enableValidation();
+
+const addValidator = new FormValidator(validationConfig, popupAddForm);
+addValidator.enableValidation();
+
+/** Открытие и закрытие попапов */
+function openPopup(popup) {
+    popup.classList.add('popup_opened');
+    document.addEventListener('keydown', closeOnEscape);
+}
+
+function closePopup(popup) {
+    popup.classList.remove('popup_opened');
+    document.removeEventListener('keydown', closeOnEscape);
+}
+
+popupList.forEach(popup => {
+    popup.addEventListener('click', (evt) => {
+        if(evt.target && evt.target.matches('.popup_opened') || evt.target.matches('.popup__close-button')) {
+            closePopup(popup);
+        }
+    });
+})
+
+function closeOnEscape(evt) {
+    if (evt.key === 'Escape') {
+        const openedPopup = document.querySelector('.popup_opened');
+        closePopup(openedPopup);
+    };
+}
+
+/** Работа с edit модальным окном */
+function openEditPopup() {
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileAbout.textContent;
+    editValidator.makeDisabledBtn();
+    editValidator.resetErrors();
+    openPopup(popupEdit);
+}
+
+function handleProfileFormSubmit(evt) {
+    evt.preventDefault();
+    profileName.textContent = nameInput.value;
+    profileAbout.textContent = jobInput.value;
+    closePopup(popupEdit);
+}
+
+editBtn.addEventListener('click', openEditPopup);
+formElementEdit.addEventListener('submit', handleProfileFormSubmit);
+
+/** Создание карточек */
+function createCard(data) {
+    const card = new Card(data, '.gallery__photos', openPhoto);
+    return card.generateCard();
+}
+
+/** Создание карточек из базы */
+initialCards.forEach((element) => {
+    photoCardsContainer.prepend(createCard(element));
+});
+
+/** Открытие фото по её нажатию */
+function openPhoto(name, link) {
+    photo.src = link;
+    photo.alt = name;
+    caption.textContent = name;
+    openPopup(popupArea);
+}
+
+/** Работа с add -form и -модальным окном */
+popupAddForm.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+
+    const addFormValue = {
+        name: nameAddInput.value,
+        link: linkAddInput.value,
+    }
+
+    photoCardsContainer.prepend(createCard(addFormValue));
+    evt.target.reset();
+
+    closePopup(popupAdd);
+});
+
+modalAddBtn.addEventListener('click', (evt) => {
+    openPopup(popupAdd);
+    addValidator.makeDisabledBtn();
+    addValidator.resetErrors();
+    popupAddForm.reset();
+});
---
+++ src/utils/constants.js
@@ -0,0 +1,57 @@
+export const initialCards = [
+    {
+        name: 'Байкал',
+        link: "https://images.unsplash.com/photo-1614357932292-a38393b966a3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2000&q=80"
+    },
+    {
+        name: 'Териберка',
+        link: 'https://images.unsplash.com/photo-1606841599773-7307a2b5ce34?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1306&q=80'
+    },
+    {
+        name: 'Сулакский каньон, Дагестан',
+        link: 'https://images.unsplash.com/photo-1617573543793-1b13d0a3f75c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
+    },
+    {
+        name: 'Халактырский пляж, Камчатка',
+        link: 'https://images.unsplash.com/photo-1557094005-176cbfe3554d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2668&q=80'
+    },
+    {
+        name: 'Карелия',
+        link: 'https://images.unsplash.com/photo-1638739948407-64ee2dc45e9c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1548&q=80'
+    },
+    {
+        name: 'Эльбрус',
+        link: 'https://images.unsplash.com/photo-1521311587563-6a3fb9fbaff7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
+    }
+];
+
+export const validationConfig = {
+    formSelector: '.popup__form',
+    inputSelector: '.popup__input',
+    submitButtonSelector: '.popup__save',
+    inactiveButtonClass: 'popup__save_inactive',
+    inputErrorClass: 'popup__input_type-error',
+    errorClass: 'popup__text-error_visible'
+};
+
+export const popupList = document.querySelectorAll('.popup');
+
+export const popupEdit = document.querySelector('.popup_edit');
+export const formElementEdit = document.querySelector('.popup__edit-form');
+export const nameInput = document.querySelector('.popup__input_edit_name');
+export const jobInput = document.querySelector('.popup__input_edit_about');
+export const profileName = document.querySelector('.profile__name');
+export const profileAbout = document.querySelector ('.profile__about');
+export const editBtn = document.querySelector('.profile__edit-button');
+
+export const modalAddBtn = document.querySelector('.profile__add-button');
+export const popupAdd = document.querySelector('.popup_add');
+export const popupAddForm = document.querySelector('.popup__add-form');
+export const nameAddInput = popupAddForm.querySelector('.popup__input_add_name');
+export const linkAddInput = popupAddForm.querySelector('.popup__input_add_link');
+
+export const photoCardsContainer = document.querySelector('.gallery');
+
+export const popupArea = document.querySelector('.popup_place-photo');
+export const photo = popupArea.querySelector('.popup__photo');
+export const caption = popupArea.querySelector('.popup__photo-caption');
