f19363904ec72e25946f2865138e3a519a742c2e
Anastasia Kosataya <102319144+akosataya@users.noreply.github.com>
2022-12-13 17:23:41 -0800

    refactor: changed HTML and added class Api

--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,11 @@ module.exports = {
                 },
                     'postcss-loader']
             },
+            {
+                test: /\.js$/,
+                enforce: "pre",
+                use: ["source-map-loader"],
+            },
         ]
     },
     plugins: [
