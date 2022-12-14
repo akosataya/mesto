beb09c60d0407e1e652ebb562c3f05abf9564652
Anastasia Kosataya <102319144+akosataya@users.noreply.github.com>
2022-12-14 15:37:04 -0800

    refactor: added class PopupWithConfirmation

--- src/components/PopupWithConfirmation.js
+++ src/components/PopupWithConfirmation.js
@@ -1,4 +1,22 @@
-export default class PopupWithConfirmation {
-    constructor() {
+import Popup from "./Popup.js";
+
+export default class PopupWithConfirmation extends Popup {
+    constructor(popup) {
+        super(popup);
+
+        this._form = this._popup.querySelector('.popup__form');
+    }
+
+    setDeletionSubmit(handleForm) {
+        this._handleForm = handleForm;
+    }
+
+    setEventListeners() {
+        super.setEventListeners();
+
+        this._form.addEventListener('submit', (evt) => {
+            evt.preventDefault();
+            this._handleForm();
+        });
     }
 }
